45085a9616ef419cec9113aa45b4343377af83d3
Diego Maquill <diego.maquill@gmail.com>
2019-06-25 18:50:25 -0400

    add_port

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var path = require('path');
 
 // Configure the Express application
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3000;
 
 // Expose the public directory to access CSS files
 app.use(express.static(path.join(__dirname, './app/public')));
 
 // Add middleware for parsing incoming request bodies
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.text());
 
 // Add the application routes
